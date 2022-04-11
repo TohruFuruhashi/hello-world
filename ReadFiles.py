cc38fff6f23581e4fd5351d7c1b44f98900c2ae4
Tohru Furuhashi <tohru@e-spike.jp>
2022-04-11 10:23:05 +0900

    Create ReadFiles.py

---
+++ ReadFiles.py
@@ -0,0 +1,14 @@
+readFiles = glob.glob(workDir)
+
+for readFilePath in readFiles:
+
+    try:
+        readFile = open(readFilePath, 'r', encoding='UTF-8')
+    except OSError as e:
+        print("Couldn't read the file !!")
+        quit()
+
+    for data in readFile:
+        print(data)
+
+    readFile.close()
