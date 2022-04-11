d4ef7cef2a4e7fdf676243606c4151574d10ead2
Tohru Furuhashi <tohru@e-spike.jp>
2022-04-11 09:44:17 +0900

    Create DecompressGzip.py

---
+++ DecompressGzip.py
@@ -0,0 +1,6 @@
+try:
+    with gzip.open(gzipFilePath, mode="rb") as gzipFile:
+        with open(decompressedFilePath, mode="wb") as decompressedFile:
+            shutil.copyfileobj(gzipFile, decompressedFile)
+except OSError as e:
+    print("Couldn't decompress the file !!")
