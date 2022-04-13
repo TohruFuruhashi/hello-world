5d22748e6b6599ad28bb6ed69b58696b1bf46f4d
Tohru Furuhashi <tohru@e-spike.jp>
2022-04-13 17:21:21 +0900

    Update PlotStockPrice.py

--- PlotStockPrice.py
+++ PlotStockPrice.py
@@ -1,9 +1,9 @@
 import pandas_datareader.data as pdr
 import mplfinance as mpf
 
-def get_stock_data(code):
+def getStockPrice(code):
   df = pdr.DataReader("{}.JP".format(code), "stooq").sort_index()
   return df
 
-df = get_stock_data(8604).tail(100)
+df = getStockPrice(8604).tail(100)
 mpf.plot(df, type='candle', volume=True, savefig='8604_JP.png')
